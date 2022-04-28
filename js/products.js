e58ddd8e6ab7ef657f468c0e7a918e3989b57309
Anthony-Rgs <95974061+Anthony-Rgs@users.noreply.github.com>
2022-04-28 21:30:34 +0200

    feat: animation

--- js/products.js
+++ js/products.js
@@ -1,19 +1,14 @@
-// function filters(id, id2, id3) {
-//   if (document.querySelector(`#filters-${id}`).classList.contains('active')) {
-//     document.querySelector(`#filters-${id}`).classList.remove('active')
-//   } else {
-//     document.querySelector(`#filters-${id}`).classList.add('active')
-//     document.querySelector('ul').classList.add('active')
-//     document.querySelector(`#filters-${id2}`).classList.remove('active')
-//     document.querySelector(`#filters-${id3}`).classList.remove('active')
-//   }
-// }
+const details = document.querySelectorAll('details')
+const span = document.querySelector('span')
 
-const details = [...document.querySelectorAll('details')]
-console.log(details)
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener('click', () => {
+    span.classList.add('active')
 
-details.map((detail) => {
-  detail.addEventListener('click', (e) => {
-    e.target.classList.add('active')
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute('open')
+      }
+    })
   })
 })
