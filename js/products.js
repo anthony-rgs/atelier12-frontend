2825c9a0b4f0dbd487f8e4fe5f367dd80dc518b8
Anthony-Rgs <95974061+Anthony-Rgs@users.noreply.github.com>
2022-04-30 17:39:11 +0200

    feat: template

---
+++ js/products.js
@@ -0,0 +1,30 @@
+const baseUrl = 'http://localhost:1337'
+
+fetch(`${baseUrl}/api/produits?populate=*`)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.data[0].attributes.image.data[0].attributes)
+    data.data.forEach((productData) => {
+      createProductElement(productData)
+    })
+  })
+
+const template = document.querySelector('template#product')
+const products = document.querySelector('.products')
+
+function createProductElement(productData) {
+  const clone = template.content.cloneNode(true)
+  const img = clone.querySelector('#product-image')
+  const name = clone.querySelector('#name')
+  const price = clone.querySelector('#price')
+  const describe1 = clone.querySelector('#describe1')
+  const describe2 = clone.querySelector('#describe2')
+
+  img.src = `${baseUrl + productData.attributes.image.data[0].attributes.url}`
+  name.textContent = productData.attributes.marque
+  price.textContent = `${productData.attributes.prix} €`
+  describe1.textContent = productData.attributes.descriptif1
+  describe2.textContent = productData.attributes.descriptif2
+
+  products.appendChild(clone)
+}
