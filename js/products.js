483535ef85b2e530986ac27850b5a8a70dcc922c
Anthony-Rgs <95974061+Anthony-Rgs@users.noreply.github.com>
2022-05-08 01:19:34 +0200

    feat: filter bar on click and product page js

--- js/products.js
+++ js/products.js
@@ -5,8 +5,13 @@ fetch(`${baseUrl}/api/produits?populate=*`)
   .then((data) => {
     console.log(data)
     data.data.forEach((productData) => {
-      createProductElement(productData)
-      productPage(productData)
+      const productElement = createProductElement(productData)
+      console.log(productElement)
+
+      productElement.addEventListener('click', () => {
+        page.classList.add('open')
+        productPage(productData)
+      })
     })
   })
 
@@ -16,8 +21,8 @@ const products = document.querySelector('.products')
 function createProductElement(productData) {
   const clone = template.content.cloneNode(true)
   const img = clone.querySelector('#product-image')
-  const brand = clone.querySelector('#brand')
-  const price = clone.querySelector('#price')
+  const brand = clone.querySelector('#brand-product')
+  const price = clone.querySelector('#price-product')
   const describe1 = clone.querySelector('#describe1')
   const describe2 = clone.querySelector('#describe2')
 
@@ -28,6 +33,7 @@ function createProductElement(productData) {
   describe2.textContent = productData.attributes.descriptif2
 
   products.appendChild(clone)
+  return Array.from(products.querySelectorAll('.product')).at(-1)
 }
 
 function productPage(productData) {
@@ -38,7 +44,6 @@ function productPage(productData) {
   const quantity = document.querySelector('#show-quantity')
   const describe1 = document.querySelector('#show-describe1')
   const describe2 = document.querySelector('#show-describe2')
-  const id = document.querySelector('#id')
 
   img.src = `${baseUrl + productData.attributes.image.data[0].attributes.url}`
   nameProduct.textContent = productData.attributes.nom
@@ -47,7 +52,6 @@ function productPage(productData) {
   quantity.textContent = productData.attributes.quantite
   describe1.textContent = productData.attributes.descriptif1
   describe2.textContent = productData.attributes.descriptif2
-  id.textContent = productData.attributes.id
 }
 
 const cross = document.querySelector('#cross')
@@ -59,4 +63,37 @@ function openProduct() {
 
 cross.addEventListener('click', () => {
   page.classList.remove('open')
+})
+
+// Filter Open / Close
+const filterCategory = document.querySelector('#filter-category')
+const filterPrice = document.querySelector('#filter-price')
+const spanCategory = document.querySelector('#span-category')
+const spanPrice = document.querySelector('#span-price')
+const ulCategory = document.querySelector('#ul-category')
+const ulPrice = document.querySelector('#ul-price')
+
+filterCategory.addEventListener('click', () => {
+  spanPrice.classList.remove('filter')
+  ulPrice.classList.remove('filter')
+
+  if (spanCategory.classList.contains('filter')) {
+    spanCategory.classList.remove('filter')
+    ulCategory.classList.remove('filter')
+  } else {
+    spanCategory.classList.add('filter')
+    ulCategory.classList.add('filter')
+  }
+})
+
+filterPrice.addEventListener('click', () => {
+  spanCategory.classList.remove('filter')
+  ulCategory.classList.remove('filter')
+  if (spanPrice.classList.contains('filter')) {
+    spanPrice.classList.remove('filter')
+    ulPrice.classList.remove('filter')
+  } else {
+    spanPrice.classList.add('filter')
+    ulPrice.classList.add('filter')
+  }
 })
